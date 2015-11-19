45177ba2322c9c7df2ac826da3c97b6ab47387a6
Etienne42 <etienne@passwordbox.com>
2015-11-19 18:23:03 -0500

    add support for status

---
+++ status.go
@@ -0,0 +1,19 @@
+package testrail
+
+type Statuses struct {
+	ColorBright int    `json:"color_bright"`
+	ColorDark   int    `json:"color_dark"`
+	ColorMedium int    `json:"color_medium"`
+	ID          int    `json:"id"`
+	IsFinal     bool   `json:"is_final"`
+	IsSystem    bool   `json:"is_system"`
+	IsUntested  bool   `json:"is_untested"`
+	Label       string `json:"label"`
+	Name        string `json:"name"`
+}
+
+func (c *Client) GetStatuses() ([]Statuses, error) {
+	returnStatuses := []Statuses{}
+	err := c.sendRequest("GET", "get_statuses/", nil, &returnStatuses)
+	return returnStatuses, err
+}
