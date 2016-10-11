4cb42ad22a19c4c2a6ba85a1509d3fd4121073de
Gavrie Philipson <gavrie@philipson.co.il>
2016-10-11 11:23:53 +0300

    Add support for TestRail timespan type
    Support TestRail's string-based timestamp types:
    - Allow converting values such as "1w 2d" to and from native time.Duration values
    - Add a unit test for marshaling and unmarshaling

---
+++ timespan_test.go
@@ -0,0 +1,75 @@
+package testrail
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTimespanUnmarshal(t *testing.T) {
+	var testData = []struct{ json, stringDuration string }{
+		{`null`, "0s"},
+		{`"15s"`, "15s"},
+		{`"12m"`, "12m"},
+		{`"11h"`, "11h"},
+		{`"4h 5m 6s"`, "4h5m6s"},
+		{`"1d"`, "8h"},
+		{`"1w"`, "40h"},
+		{`"1d 2h"`, "8h2h"},
+		{`"1w 2d 3h"`, "40h16h3h"},
+	}
+
+	for _, data := range testData {
+		var results []Result
+		js := []byte(fmt.Sprintf(`[{"elapsed":%v}]`, data.json))
+		if err := json.Unmarshal(js, &results); err != nil {
+			t.Fatal(err)
+		}
+
+		r := results[0]
+		expected, err := time.ParseDuration(data.stringDuration)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if r.Elapsed.Duration != expected {
+			t.Fatalf("Wrong duration: %v", r.Elapsed.Duration)
+		}
+	}
+}
+
+func TestTimespanMarshal(t *testing.T) {
+	var testData = []struct{ json, stringDuration string }{
+		{`null`, "0s"},
+		{`"0h 0m 15s"`, "15s"},
+		{`"0h 12m 0s"`, "12m"},
+		{`"11h 0m 0s"`, "11h"},
+		{`"4h 5m 6s"`, "4h5m6s"},
+		{`"8h 0m 0s"`, "8h"},
+		{`"40h 0m 0s"`, "40h"},
+		{`"10h 0m 0s"`, "8h2h"},
+		{`"59h 0m 0s"`, "40h16h3h"},
+	}
+
+	for _, td := range testData {
+		duration, err := time.ParseDuration(td.stringDuration)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		result := Result{
+			Elapsed: timespan{
+				Duration: duration,
+			},
+		}
+		data, err := json.Marshal(result.Elapsed)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if string(data) != td.json {
+			t.Fatalf("Wrong data: %v", string(data))
+		}
+	}
+}
